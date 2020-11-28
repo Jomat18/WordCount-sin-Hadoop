b87f5c3f2bb47e49eddd6484601ee2674c8f434f
Jomat18 <manu.jose.torres@gmail.com>
2020-11-27 22:23:48 -0500

    feat: guardando datos en el thread

--- paralelo.cpp
+++ paralelo.cpp
@@ -5,29 +5,32 @@
 #include <iostream>
 #include <unordered_map> 
 #include <cmath> 
+#include <sstream>
 
 using namespace std;
 
-const char * filename = "file2.txt";
+const char * filename = "file.txt";
+FILE *file; 
 
-using namespace std;
 
-struct to_read{
-  FILE *fp;
-  long int offset;
-  long int start;
-  long int chunk_size;
-  unordered_map<string, int> umap; 
-};
+typedef struct thread {   
+	pthread_t thread_id;       
+	FILE *output;
+	long int offset;
+	long int start;
+	long int chunk_size;
+	unordered_map<string, int> umap; 
+} ThreadData;
 
-void *wordCount(void *arg)
+void *wordCount(void *thread)
 {
-  struct to_read *data = (struct to_read *)arg;
+  ThreadData *data  = (ThreadData*)thread;	
+  //struct to_read *data = (struct to_read *)arg;
 
   char *buffer = (char*) malloc (sizeof(char)*data->chunk_size);
 
-  fseek(data->fp, data->start, data->offset);
-  fread(buffer, data->chunk_size, 1, data->fp);
+  fseek(file, data->start, data->offset);
+  fread(buffer, data->chunk_size, 1, file);
 
   char *token = strtok(buffer, " "); 
   
@@ -42,21 +45,20 @@ void *wordCount(void *arg)
       token = strtok(NULL, " "); 
   } 
 
-  for (auto x : data->umap) 
-		cout<<x.first<<" "<<x.second<<endl; 
-
   free(buffer);
   free(token);
-  pthread_exit(NULL);
+  //free(word);
+  //pthread_exit(NULL);
+  return NULL;
 
 }
 
-int main( int argc, char **argv )
+int main() 
 { 
-    FILE *fp; 
-    fp = fopen(filename, "r"); 
 
-    if (fp==NULL)
+    file = fopen(filename, "r"); 
+
+    if (file==NULL)
     {
       perror("Could not open file.");
       exit(1);
@@ -64,14 +66,13 @@ int main( int argc, char **argv )
 
     long int size, chunk_size;
     int nthreads, ret_val, tid;
-    pthread_t *threads;
 
     printf("Enter the number of threads: ");
 	scanf("%d",&nthreads);
-	threads = (pthread_t *)malloc(nthreads*sizeof(pthread_t));
+	//threads = (pthread_t *)malloc(nthreads*sizeof(pthread_t));
 
-	fseek(fp, 0L, SEEK_END); //OL
-    size = ftell(fp);  
+	fseek(file, 0L, SEEK_END); //OL
+    size = ftell(file);  
 
     chunk_size = ceil((1.0*size)/nthreads);
 
@@ -79,37 +80,71 @@ int main( int argc, char **argv )
    	printf ("Size of chunk_size: %ld bytes.\n\n",chunk_size);
    
 
-    fseek(fp, 0, SEEK_SET); 
+    fseek(file, 0, SEEK_SET); 
+    int output_files = 1;
+  	string temp;
+
+  	ThreadData thread[nthreads]; 
 
 	for(tid = 0; tid < nthreads; tid++)
     {
-    	struct to_read data;
+    	FILE *output;
+	    stringstream ss;
+	    ss << output_files;
+	    
+	    temp = "output_" + ss.str() + ".txt";
+	    output = fopen(temp.c_str(),"w");
 
-		data.fp=fp;
-		data.chunk_size=chunk_size;
-		data.start = tid*chunk_size;
-		data.offset = data.start + chunk_size;
+	    thread[tid].output = output;
+		thread[tid].chunk_size = chunk_size;
+		thread[tid].start = tid*chunk_size;
+		thread[tid].offset = thread[tid].start + chunk_size;
 
-		ret_val = pthread_create(&threads[tid], NULL, wordCount, (void*) &data);
+		ret_val = pthread_create(&(thread[tid].thread_id), NULL, wordCount, (void *)(thread+tid));
       
 		if(ret_val!= 0) {
 			printf ("Create pthread error!\n");
 			exit (1);
-      }
+      	}
+      	output_files++;
     }
 
 
 	//wait for results (all threads)
 	for (tid = 0; tid < nthreads; tid++){
-		pthread_join(threads[tid], NULL);
+		pthread_join(thread[tid].thread_id, NULL);
 	}  
+	
+    for (tid = 0; tid < nthreads; tid++){
 
-	fclose(fp);
-	free(threads);
+		  if(thread[tid].output == NULL)
+		  {
+		    printf("Error!");   
+		    exit(1);             
+		  }
 
-	pthread_exit(NULL);
+
+		  char *word;
+
+		  for (auto x : thread[tid].umap) {
+
+		    word = (char*) malloc (sizeof(char)*(x.first.length()+1));
+		    strcpy(word, x.first.c_str());  
+
+		    fprintf(thread[tid].output, "%s %d\n",word,x.second);
+		  }
+
+		free(word); 	
+        fclose(thread[tid].output);
+    }
+
+	fclose(file);
+	//free(thread);
+
+	//pthread_exit(NULL);
   
     return 0; 
 } 
 
 //g++ paralelo.cpp -o wordcount -lpthread
+
