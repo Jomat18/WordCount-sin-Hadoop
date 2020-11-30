00d5a57153063ea3a678e070c20e8fcc7a3ff44e
Jomat18 <manu.jose.torres@gmail.com>
2020-11-29 20:16:25 -0500

    feat: v2.1

--- parallel.cpp
+++ parallel.cpp
@@ -1,158 +1,160 @@
 #include <string.h>
-#include <stdio.h> 
-#include <stdlib.h>
-#include <pthread.h>
 #include <unordered_map> 
 #include <sstream>
 #include <math.h>
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 pthread_mutex_t lock;
 unordered_map<string, int> ureduce; 
 
-typedef struct thread {   
-	pthread_t thread_id;       
-  FILE *input;
-  ofstream output;
-  char *token;
-  char *buffer;
-	long size;
-	unordered_map<string, int> umap; 
+typedef struct thread_data {   
+    int thread_id;       
+    FILE *input;
+    ofstream output;
+    char *token;
+    char *buffer;
+    int size;
+    unordered_map<string, int> umap; 
 } ThreadData;
 
 
-void *wordCount(void *thread)
+void *wordCount(void *threadarg)
 {
-
-  ThreadData *data  = (ThreadData*)thread;	
-
-  //printf("%s %ld %li\n", data->buffer, data->thread_id, data->size);
-
-  data->token = strtok(data->buffer, " \n\t"); 
+    ThreadData *data;
+    data  = (ThreadData*) threadarg;
   
-  while (data->token != NULL) 
-  {  
-      if (data->umap.find(data->token) == data->umap.end()) 
-        data->umap[data->token] = 1;
+    data->token = strtok(data->buffer, " \n\t"); 
+  
+    while (data->token != NULL) 
+    {  
+        if (data->umap.find(data->token) == data->umap.end()) 
+            data->umap[data->token] = 1;
           
-      else
-        data->umap.at(data->token) += 1;
+        else
+            data->umap.at(data->token) += 1;
 
-      data->token = strtok(NULL, " \n\t");
-  } 
+        data->token = strtok(NULL, " \n\t");
+    } 
 
-  free(data->buffer);
-  free(data->token);
-  return NULL;
-
+	free(data->token);   
+	free(data->buffer);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
-{ 
-
-    if(argc<3){
-      perror("./wordcount number_files nthreads\n");
-      exit(1);	
+{
+    if(argc<3)
+    {
+        perror("./wordcount number_files nthreads\n");
+        exit(1);  
     }
 
     int nthreads, residue, n_iterations, ret_val, tid, output_files = 1;
     string temp;
 
     nthreads = atoi(argv[2]);
-
     residue = atoi(argv[1])%nthreads;
-
     n_iterations = ceil(atof(argv[1])/nthreads);    
+    ThreadData thread[nthreads]; 
 
     printf ("\nNumber of threads: %d\n", nthreads);
     printf ("Number of files: %i\n", atoi(argv[1]));
     printf ("Number of iterations: %i\n\n", n_iterations);
 
-  for (int it = 0; it < n_iterations; it++)
-  {   
-      if (it == n_iterations-1 && residue)
-        nthreads = residue;
+    pthread_t threads[nthreads];
 
-      ThreadData thread[nthreads]; 
+    for (int it = 0; it < n_iterations; it++)
+    {   
+        if (it == n_iterations-1 && residue)
+            nthreads = residue;
 
-    	for(tid = 0; tid < nthreads; tid++)
-      {
-
-    	    stringstream ss;
-    	    ss << output_files;
-    	    
-    	    temp = "output/output_" + ss.str() + ".txt";
-    	    thread[tid].output.open(temp.c_str(), ios_base::app); 
-
-          temp = "chunks/input_" + ss.str() + ".txt";
-          thread[tid].input = fopen(temp.c_str(), "rb");
-
-          if (!thread[tid].input | !thread[tid].output)
-          {
-            perror("Could not open file.\n");
-            exit(1);
-          }
-
-          fseek(thread[tid].input, 0, SEEK_END); 
-          thread[tid].size = ftell(thread[tid].input);  
-
-          fseek(thread[tid].input, 0, SEEK_SET); 
-
-
-          thread[tid].buffer = (char*) malloc (sizeof(char)*thread[tid].size);
-          fread(thread[tid].buffer, 1, thread[tid].size, thread[tid].input); 
-
-    		  ret_val = pthread_create(&(thread[tid].thread_id), NULL, wordCount, (void *)(thread+tid));
+        for(tid = 0; tid < nthreads; tid++)
+        {
+            stringstream ss;
+            ss << output_files;
           
-    		  if(ret_val!= 0) {
-    			   printf ("Create pthread error!\n");
-    			   exit (1);
-          }
+            temp = "output/output_" + ss.str() + ".txt";
+            thread[tid].output.open(temp.c_str(), ios_base::app); 
 
-          output_files++;
-      }
+            temp = "chunks/input_" + ss.str() + ".txt";
+            thread[tid].input = fopen(temp.c_str(), "r");
 
+            if (!thread[tid].input | !thread[tid].output)
+            {
+                perror("Could not open file.\n");
+                exit(1);
+            }
 
-    	//wait for results (all threads)
-    	for (tid = 0; tid < nthreads; tid++){
-    		  pthread_join(thread[tid].thread_id, NULL);
-    	}  
-	
-      for (tid = 0; tid < nthreads; tid++){
+            fseek(thread[tid].input, 0, SEEK_END); 
+            thread[tid].size = ftell(thread[tid].input);  
 
-          fclose(thread[tid].input);
+            fseek(thread[tid].input, 0, SEEK_SET); 
 
-          pthread_mutex_lock(&lock);  
+            thread[tid].buffer = (char*) malloc (thread[tid].size*sizeof(char));
+            fread(thread[tid].buffer, 1, thread[tid].size, thread[tid].input); 
 
-    		  for (auto x : thread[tid].umap) {
+            thread[tid].thread_id = tid;
 
-            if (ureduce.find(x.first) == ureduce.end()) 
-              ureduce[x.first] = x.second;
+            //printf("%s %d %li\n", thread[tid].buffer, thread[tid].thread_id, thread[tid].size);
+          
+            ret_val = pthread_create(&threads[tid], NULL, wordCount, (void *)&thread[tid]);
+          
+            if(ret_val) 
+            {
+               printf("ERROR; return code from pthread_create() is %d\n", ret_val);
+               exit(-1);
+            }
 
-            else
-              ureduce.at(x.first) += x.second;
+            output_files++;
+        }
 
-            thread[tid].output << x.first <<" "<<x.second<<endl;
-    		  }
+    
+        for (tid = 0; tid < nthreads; tid++)
+        {
 
-          pthread_mutex_unlock(&lock);
-          thread[tid].output.close();
-      }
+            ret_val = pthread_join(threads[tid],  NULL);
 
-      printf("Iteration number %i: %i threads\n", it+1, nthreads);
-    }  
+            if (ret_val) 
+            {
+               printf("ERROR; return code from pthread_join() is %d\n", ret_val);
+               exit(-1);
+            }
+
+            pthread_mutex_lock(&lock);  
+
+            for (auto x : thread[tid].umap) 
+            {
+
+                if (ureduce.find(x.first) == ureduce.end()) 
+                    ureduce[x.first] = x.second;
+
+                else
+                    ureduce.at(x.first) += x.second;
+
+                thread[tid].output << x.first <<" "<<x.second<<endl;
+            }
+
+            pthread_mutex_unlock(&lock);
+    		fclose(thread[tid].input);
+            thread[tid].output.close();
+        }    
+
+        printf("Iteration number %i: %i threads\n", it+1, nthreads);
+
+    }
 
     cout<<endl<<"*********** Results *************"<<endl<<endl;
 
-    for (auto x : ureduce) {
+    for (auto x : ureduce) 
+        cout<<x.first<<" "<<x.second<<endl;
 
-      cout<<x.first<<" "<<x.second<<endl;
-    }  
-
-    //pthread_exit(NULL);  
+    pthread_exit(NULL);  
   
-    return 0; 
 } 
--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+FILE=input/file.txt
+N_LINES_CHUNK=400
+
 g++ generate_file.cpp -o gfile
 ./gfile
 
-FILE=input/file.txt
-
-N_LINES="$(wc -l $FILE | awk '{ print $1 }')";
+N_LINES=$(wc -l <${FILE});
 
 let N_LINES=N_LINES+1
 
 echo "Number of lines of file.txt "$N_LINES
 
-split -l 100 input/file.txt chunks/chunk_.txt
+split -l $N_LINES_CHUNK -e $FILE chunks/chunk_
+
+echo "Number of lines of chunk files "$N_LINES_CHUNK
 
 counter=1; 
 
@@ -19,9 +22,11 @@ for file in chunks/*; do
 	[[ -f $file ]] && mv -i "$file" "chunks/input"_$((counter))."txt" && ((counter++)); 
 done
 
-NUMBER_FILES="$(ls chunks | wc -l)";
+#NUMBER_FILES="$(ls chunks | wc -l)";
+
+let counter=counter-1
 
 N_THREADS=4
 
-g++ parallel.cpp -o wordcount -lpthread
-./wordcount $NUMBER_FILES $N_THREADS
+g++ parallel.cpp -o wordcount -pthread
+./wordcount $counter $N_THREADS
