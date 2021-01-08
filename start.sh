22e9f96e5638bd22f82e0b466d30914b38ff8a31
jomat18 <manu.jose.torres@gmail.com>
2021-01-08 04:44:33 -0500

    feat: script v2.2

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+#FILE=$1
+#[ -f "$FILE" ] || exit
+
 FILE=input/file.txt
+FILE_CHUNK=chunks/input.txt
 
 #awk '/MemTotal/ {print $2}' /proc/meminfo
 #cat /proc/meminfo | grep MemTotal
+
 #awk '/MemFree/ {print $2}' /proc/meminfo
 #awk '/MemAvailable/ {print $2}' /proc/meminfo
 # free  // kilobytes
 # free -m   //megabyte
-
-
-N_LINES_CHUNK=400
 
 g++ generate_file.cpp -o gfile
 ./gfile
@@ -21,19 +23,38 @@ let N_LINES=N_LINES+1
 
 echo "Number of lines of file.txt "$N_LINES
 
-split -l $N_LINES_CHUNK -e $FILE chunks/chunk_
+BYTES=0;
+counter=0; 
+N_LINES_CHUNK=200;
 
-echo "Number of lines of chunk files "$N_LINES_CHUNK
+echo "Number of lines of chunk "$N_LINES_CHUNK
 
-counter=1; 
+> $FILE_CHUNK
 
-for file in chunks/*; do 
-	[[ -f $file ]] && mv -i "$file" "chunks/input"_$((counter))."txt" && ((counter++)); 
+for LINE in $(seq 1 $N_LINES); do
+	let BYTES+=$(sed -n "$LINE"p "$FILE" | wc -c)
+	
+    if [ $(($LINE%$N_LINES_CHUNK)) -eq 0 ] || [ "$LINE" -eq $N_LINES ]
+    then
+		echo $BYTES >> $FILE_CHUNK
+		BYTES=0
+		((counter++))
+	fi	
 done
 
-#NUMBER_FILES="$(ls chunks | wc -l)";
 
-let counter=counter-1;
+
+#split -l $N_LINES_CHUNK -e $FILE chunks/chunk_
+
+#counter=1; 
+
+#for file in chunks/*; do 
+#	[[ -f $file ]] && mv -i "$file" "chunks/input"_$((counter))."txt" && ((counter++)); 
+#done
+
+## NUMBER_FILES="$(ls chunks | wc -l)";
+
+#let counter=counter-1;
 
 #nproc
 N_THREADS=$(nproc --all);  #4
